0f1381120ec5cb0ddcc32c8e5fa86f163c4a7479
jhelf <jean.helfenstein@gmail.com>
2022-12-13 11:18:50 -0800

    update

--- server.js
+++ server.js
@@ -15,10 +15,15 @@ const wss = new Server({ server });
 wss.on('connection', (ws) => {
   console.log('Client connected');
   ws.on('close', () => console.log('Client disconnected'));
+  ws.on('message', (data) => {
+    wss.clients.forEach((client) => {
+      client.send(data);
+    });
+  });
 });
 
-setInterval(() => {
-  wss.clients.forEach((client) => {
-    client.send(new Date().toTimeString());
-  });
-}, 1000);
+// setInterval(() => {
+//   wss.clients.forEach((client) => {
+//     client.send(new Date().toTimeString());
+//   });
+// }, 1000);
